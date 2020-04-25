56dc7ba7b483209dad82835bf1580ee1ce71b58d
artem.bondarchuk <arzek2015@gmail.com>
2020-04-25 18:45:28 +0300

    Cars e2e

--- test/cars.e2e-spec.ts
+++ test/cars.e2e-spec.ts
@@ -1,54 +1,21 @@
 import * as request from 'supertest';
-import * as dotenv from 'dotenv';
 import * as faker from 'faker';
 
-dotenv.config();
-
-import { Test } from '@nestjs/testing';
-
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-
-import { AppModule } from '../src/app.module';
+import { createCar, createTestingServer } from './utils';
+import { INestApplication } from '@nestjs/common';
 
 describe('CarsController (e2e)', () => {
   let app: INestApplication;
   let server;
 
-  const car = {
-    price: faker.random.number(),
-    firstRegistrationDate: '2020-04-25T14:39:45.273Z',
-    manufacturer: {
-      name: faker.name.findName(),
-      phone: '+380994154440',
-      siret: faker.random.number(),
-    },
-    owners: [
-      {
-        name: faker.name.findName(),
-        purchaseDate: '2020-04-25T14:39:45.273Z',
-      },
-    ],
-  };
-
-  const createCar = () =>
-    request(server)
-      .post('/cars')
-      .send(car)
-      .expect(201);
-
   beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe({ transform: true }));
-    await app.init();
-    server = app.getHttpServer();
+    const testingServer = await createTestingServer();
+    server = testingServer.server;
+    app = testingServer.app;
   });
 
   it('POST /cars (success)', async () => {
-    return createCar();
+    return createCar(server);
   });
 
   it('POST /cars (error)', async () => {
@@ -59,7 +26,7 @@ describe('CarsController (e2e)', () => {
   });
 
   it('GET /cars', async () => {
-    await createCar();
+    await createCar(server);
     const { body } = await request(server)
       .get('/cars')
       .expect(200);
@@ -68,7 +35,7 @@ describe('CarsController (e2e)', () => {
   });
 
   it('GET /cars/{id} (success)', async () => {
-    const { body } = await createCar();
+    const { body } = await createCar(server);
     return request(server)
       .get(`/cars/${body.id}`)
       .expect(200);
@@ -81,7 +48,7 @@ describe('CarsController (e2e)', () => {
   });
 
   it('GET /cars/manufacturer/{manufacturerId} (success)', async () => {
-    const { body } = await createCar();
+    const { body } = await createCar(server);
     return request(server)
       .get(`/cars/manufacturer/${body.manufacturer.id}`)
       .expect(200);
@@ -94,7 +61,7 @@ describe('CarsController (e2e)', () => {
   });
 
   it('PUT /cars/{id} (success)', async () => {
-    const { body } = await createCar();
+    const { body } = await createCar(server);
 
     const newCar = {
       price: faker.random.number(),
@@ -120,7 +87,7 @@ describe('CarsController (e2e)', () => {
   });
 
   it('DELETE /cars/{id}', async () => {
-    const { body } = await createCar();
+    const { body } = await createCar(server);
 
     await request(server)
       .delete(`/cars/${body.id}`)
---
+++ test/utils.ts
@@ -0,0 +1,42 @@
+import * as faker from 'faker';
+import * as request from 'supertest';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+export const createTestingServer = async () => {
+  const moduleFixture = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  const app = moduleFixture.createNestApplication();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.init();
+  return { app, server: app.getHttpServer() };
+};
+
+export const car = {
+  price: faker.random.number(),
+  firstRegistrationDate: '2020-04-25T14:39:45.273Z',
+  manufacturer: {
+    name: faker.name.findName(),
+    phone: '+380994154440',
+    siret: faker.random.number(),
+  },
+  owners: [
+    {
+      name: faker.name.findName(),
+      purchaseDate: '2020-04-25T14:39:45.273Z',
+    },
+  ],
+};
+
+export const createCar = server =>
+  request(server)
+    .post('/cars')
+    .send(car)
+    .expect(201);
